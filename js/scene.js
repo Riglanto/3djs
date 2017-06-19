430223f62dcb699901423569b0069a770fad8747
Radoslaw Iglantowicz <jaaroos@gmail.com>
2017-06-19 11:25:37 +0200

    Model, font introduced with lighting, clean up

--- js/scene.js
+++ js/scene.js
@@ -1,33 +1,81 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var scene, camera;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3(-1, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 1, 0));
-geometry.vertices.push(new THREE.Vector3(1, 0, 0));
-
-var line = new THREE.Line(geometry, material);
-
-scene.add(line);
-
-var render = function () {
-    requestAnimationFrame( render );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
-};
+init();
 
 render();
+
+function init() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(5, 5, 10);
+    camera.lookAt(scene.position);
+
+    var axes = new THREE.AxisHelper(1);
+    scene.add(axes);
+
+    var gridHelper = new THREE.GridHelper(10, 20);
+    scene.add(gridHelper);
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc);
+    scene.add(ambientLight);
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(0, 1, -1).normalize();
+    scene.add(directionalLight);
+
+    loadModel();
+
+    loadFont();
+
+    if (Detector.webgl) {
+        renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    } else {
+        renderer = new THREE.CanvasRenderer();
+    }
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function loadModel() {
+    var colladaLoader = new THREE.ColladaLoader();
+    colladaLoader.options.convertUpAxis = true;
+    colladaLoader.load('./models/hyena.dae', function (collada) {
+        var object = collada.scene;
+        object.position.set(0, 0, 0);
+        scene.add(object);
+    });
+}
+
+function loadFont() {
+    var loader = new THREE.FontLoader();
+    loader.load('./fonts/IndieFlower.json', function (x) {
+
+        var textGeo = new THREE.TextGeometry("R", {
+            font: x,
+            size: 5,
+            height: 2,
+            // curveSegments: 12,
+            // bevelThickness: 2,
+            // bevelSize: 5,
+            // bevelEnabled: true
+        });
+        var textMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        var mesh = new THREE.Mesh(textGeo, textMaterial);
+        mesh.position.set(0, 0, 0);
+        scene.add(mesh);
+    });
+}
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
